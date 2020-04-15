331dd852f9217ae0d1366ad1b27b574f9b6cde02
ueren97 <43664634+ueren97@users.noreply.github.com>
2020-04-15 20:47:27 +0900

    add best

---
+++ best/kmnist_cnn_best.py
@@ -0,0 +1,86 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.utils import np_utils
+from keras import backend as K
+import keras
+import numpy as np
+
+batch_size = 128
+num_classes = 10
+epochs = 22
+img_rows, img_cols = 28, 28
+
+
+def load(f):
+    return np.load(f)['arr_0']
+
+
+X_train = load('../dataset/kmnist-train-imgs.npz')
+X_test = load('../dataset/kmnist-test-imgs.npz')
+y_train = load('../dataset/kmnist-train-labels.npz')
+y_test = load('../dataset/kmnist-test-labels.npz')
+
+if K.image_data_format() == 'channels_first':
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+X_train = X_train.astype('float32') / 255
+X_test = X_test.astype('float32') / 255
+print('{} train samples, {} test samples'.format(len(X_train), len(X_test)))
+
+y_train = np_utils.to_categorical(y_train, num_classes)
+y_test = np_utils.to_categorical(y_test, num_classes)
+
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+opt = keras.optimizers.Adadelta()
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=opt, metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(X_test, y_test))
+
+# モデルの保存
+# model.save('model/kmnist_cnn.h5')
+
+# return model
+
+
+train_score = model.evaluate(X_train, y_train, verbose=0)
+test_score = model.evaluate(X_test, y_test, verbose=0)
+
+print('Train loss:', train_score[0])
+print('Train accuracy:', train_score[1])
+print('Test loss:', test_score[0])
+print('Test accuracy:', test_score[1])
